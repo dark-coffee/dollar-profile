09a656010d38f53da74132ff785870821d7e0ef1
Andy <9117837+dark-coffee@users.noreply.github.com>
2021-05-20 08:10:35 +0100

    Update Microsoft.PowerShell_profile.ps1

--- Microsoft.PowerShell_profile.ps1
+++ Microsoft.PowerShell_profile.ps1
@@ -1,4 +1,18 @@
+#Version 0.1.0
+#Updated 2021-05-20
+
 function Prompt {
-[Environment]::CurrentDirectory = (Get-Location -PSProvider FileSystem | Select-Object -ExpandProperty Path);
-$env:COMPUTERNAME + "\" + (Get-Location) + "> "
+
+    #Force .NET and PS Dirs to match
+    [Environment]::CurrentDirectory = (Get-Location -PSProvider FileSystem | Select-Object -ExpandProperty Path);
+    $env:COMPUTERNAME + "\" + (Get-Location) + "> "
 }
+
+<# notes:
+* install modules?
+** getdirectory
+** dbatools
+
+* profile colours
+* oh my posh?
+#>
