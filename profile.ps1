c40d5e9ec307d1367483b7890095455444e55e9d
Andy <9117837+dark-coffee@users.noreply.github.com>
2021-06-01 15:40:02 +0100

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -1,32 +1,32 @@
-#Version 0.1.7
+#Version 0.1.8
 #Updated 2021-06-01
 
 #Clear the gubbins and inform usr of update check
-Clear-Host
-Write-Host 'Welcome back, Andy!'
-Write-Host '----------------------------------'
-Write-Host 'Checking for profile updates . . .'
+Clear-Host;
+Write-Host 'Welcome back, Andy!';
+Write-Host '----------------------------------';
+Write-Host 'Checking for profile updates . . .';
 
 #Profile URL
-$GithubFileURL = 'https://raw.githubusercontent.com/dark-coffee/dollar-profile/main/profile.ps1'
+$GithubFileURL = 'https://raw.githubusercontent.com/dark-coffee/dollar-profile/main/profile.ps1';
 
 #Define Files
 $GithubFile = Invoke-WebRequest $GithubFileURL | Select-Object -ExpandProperty Content;
 $ProfileFile = Get-Content $PROFILE;
 
 #Pull Versions from Files
-$CurrentVersion = $ProfileFile[0] -replace '#Version '
-$GitVersion = $GithubFile[9..13] | Join-String
+$CurrentVersion = $ProfileFile[0] -replace '#Version ';
+$GitVersion = $GithubFile[9..13] | Join-String;
 
 #Feed back to screen
-Write-Host ''
-Write-Host "Current profile version: $CurrentVersion"
-Write-Host "Github profile version:  $GitVersion"
-Write-Host ''
+Write-Host '';
+Write-Host "Current profile version: $CurrentVersion";
+Write-Host "Github profile version:  $GitVersion";
+Write-Host '';
 
 #Update Mechanism
 If($GitVersion -gt $CurrentVersion){
-    $UserChoice = Read-Host -Prompt 'Newer Version Found, Install? (Y)'
+    $UserChoice = Read-Host -Prompt 'Newer Version Found, Install? (Y)';
     If(($UserChoice -eq "Y") -or ($UserChoice -eq "")){
         try{
             Start-BitsTransfer -Source $GithubFileURL -Destination $PROFILE -ErrorAction Stop
@@ -38,13 +38,14 @@ If($GitVersion -gt $CurrentVersion){
         }
     }else{
         Write-Host "We'll try again next time"
-    }
+    };
 }else{
     Write-Host "Profile up-to-date!"
-}
+};
 Write-Host '----------------------------------';
 Write-Host 'Clearing host in 3 seconds . . .  ';
 Write-Host '----------------------------------';
+Start-Sleep 3;
 Clear-Host;
 
 #Prompt
@@ -53,7 +54,7 @@ function Prompt {
     #Force .NET and PS Dirs to match
     [Environment]::CurrentDirectory = (Get-Location -PSProvider FileSystem | Select-Object -ExpandProperty Path);
     $env:COMPUTERNAME + "\" + (Get-Location) + "> "
-}
+};
 
 <# notes:
 * install modules?
@@ -65,5 +66,5 @@ function Prompt {
 #>
 #Import-Module posh-git;
 Import-Module oh-my-posh;
-Set-PoshPrompt -theme hotstick.minimal;
+Set-PoshPrompt -theme stelbent.minimal;
 #Clear-Host;
